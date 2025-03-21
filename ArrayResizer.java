18f181c9e4f36b9202d6b8fc102333bf4e2a5bf9
unorderlyhamster329 <118472366+unorderlyhamster329@users.noreply.github.com>
2025-03-21 18:22:50 +0000

    pp

--- ArrayResizer.java
+++ ArrayResizer.java
@@ -6,8 +6,15 @@ public class ArrayResizer
    */
   public static boolean isNonZeroRow(int[][] array2D, int r)
   {
-    /* to be implemented in part (a) */
-    return false;
+      for (int col = 0; col < array2D[r].length; col++) {
+        if (array2D[r][col] == 0) {
+          return false;      
+        }
+      }
+
+      //if done with that row, 
+
+    return true;
   }
 
   /** Returns the number of rows of array2D that contain all nonzero values.
@@ -31,7 +38,17 @@ public class ArrayResizer
    */
   public static int[][] resize(int[][] array2D)
   {
+    int count = 0; 
+    int numRows = numNonZeroRow(array2D); //set the amount of nonzero rows there are in the array
+    int[][] resizedList = new int[numRows][]; //make a new list with the num of nonzero rows
+    for (int row = 0; row < array2D.length; row++) { 
+      if(isNonZeroRow(array2D, row)) { //loop through the array and see if there are nonzero rows
+        resizedList[count] = array2D[row]; //add the nonzero rows into the resize array
+        count++; //onto next row
+      }
+
+    }
     /* to be implemented in part (b) */
-    return null;
+    return resizedList;
   }
 }
